6453feba0e2bbf6e75726aa71dff371eb21a59eb
coleYab <yabume123@gamil.com>
2024-08-08 22:50:47 +0300

    pycodestyle fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,13 +12,13 @@ def pascal_triangle(n):
 
     for i in range(1, n + 1):
         prev = [] if i == 1 else res[-1]
-        
+
         def get_comb(prev, x, y):
-            # calculating c(x, y) = p(x) / p(x - y) * p(y) or 
+            # calculating c(x, y) = p(x) / p(x - y) * p(y)
             if x == y or x == 1 or y == 1:
                 return 1
             return prev[y - 2] + prev[y - 1]
-        
+
         res.append([get_comb(prev, i, j) for j in range(1, i + 1)])
 
     return res
